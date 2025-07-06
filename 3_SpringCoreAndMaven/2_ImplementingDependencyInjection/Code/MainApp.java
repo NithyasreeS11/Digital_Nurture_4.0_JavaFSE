f44822c9e50dba1163e75927feda040bd1e797d6
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-07-06 12:08:09 +0530

    Create MainApp.java

---
+++ 3_SpringCoreAndMaven/2_ImplementingDependencyInjection/Code/MainApp.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load Spring context from XML
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test method to verify DI works
+        bookService.addBook("To Kill a Mockingbird");
+    }
+}
