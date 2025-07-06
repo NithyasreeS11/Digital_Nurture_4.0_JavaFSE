99c4973a9fd4bca8a0ebe866ab8bb3b779140b7f
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-07-06 19:54:28 +0530

    Create EmployeeService.java

---
+++ 3_SpringDataJPAWithSpringBootHibernate/DifferenceBetweenJPAHibernateAndSpringDataJPA/SpringDataJPA/Code/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.cognizant.handson4.service;
+
+
+import com.cognizant.handson4.model.Employee;
+import com.cognizant.handson4.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
