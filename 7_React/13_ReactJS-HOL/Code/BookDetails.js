4cc32cb7521f89ddb20e3804956f1e8a22427f3b
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-08-03 17:24:09 +0530

    Create BookDetails.js

---
+++ 7_React/13_ReactJS-HOL/Code/BookDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BookDetails = () => {
+  return (
+    <div>
+      <h2>📚 Book Details</h2>
+      <p><strong>Title:</strong> React for Beginners</p>
+      <p><strong>Author:</strong> John Smith</p>
+      <p><strong>Pages:</strong> 280</p>
+    </div>
+  );
+};
+
+export default BookDetails;
