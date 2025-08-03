b69fbb28c05101d3255be1fbab23ebccfe1f9d80
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-08-03 17:24:47 +0530

    Create BlogDetails.js

---
+++ 7_React/13_ReactJS-HOL/Code/BlogDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BlogDetails = () => {
+  return (
+    <div>
+      <h2>📝 Blog Details</h2>
+      <p><strong>Title:</strong> Why React is Powerful</p>
+      <p><strong>Author:</strong> Jane Doe</p>
+      <p><strong>Date:</strong> 3 August 2025</p>
+    </div>
+  );
+};
+
+export default BlogDetails;
