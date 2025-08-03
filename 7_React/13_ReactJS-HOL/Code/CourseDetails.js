be855c2e540de2f34d77bb59ac5d90fc61201d2f
NithyasreeS11 <119521753+NithyasreeS11@users.noreply.github.com>
2025-08-03 17:25:24 +0530

    Create CourseDetails.js

---
+++ 7_React/13_ReactJS-HOL/Code/CourseDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CourseDetails = () => {
+  return (
+    <div>
+      <h2>🎓 Course Details</h2>
+      <p><strong>Course:</strong> Full Stack Web Development</p>
+      <p><strong>Instructor:</strong> Alex Johnson</p>
+      <p><strong>Duration:</strong> 6 Months</p>
+    </div>
+  );
+};
+
+export default CourseDetails;
